0e50cf74a4a786cfd420b20634b8b9783796f948
manas-anarov <you@example.com>
2020-07-22 12:00:52 +0600

    4_15

---
+++ hello3.py
@@ -0,0 +1,41 @@
+"""
+num = 3
+print(type(num))
+"""
+
+# print(3 % 2)
+# print(3 + 2)
+
+
+# print(2 % 2)
+# print(3 % 2)
+# print(4 % 2)
+# print(5 % 2)
+
+# num = 1
+# num *=10
+# print(num)
+
+# print(abs(-3))
+
+# print(round(3.17))
+
+# print(round(3.17, 1))
+
+
+# num_1= 3
+# num_2= 2
+# print(num_1 != num_2)
+
+# num_1= 3
+# num_2= 2
+# print(num_1 > num_2)
+
+
+num_1 = '100'
+num_2 = '200'
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print(num_1 + num_2)
---
+++ hello4.py
@@ -0,0 +1,140 @@
+# cources = ['History', 'Math', 'Phisics', 'CompSci']
+# print(len(cources))
+
+
+# cources = ['History', 'Math', 'Phisics', 'CompSci']
+# print(cources[-1])
+
+
+# cources = ['History', 'Math', 'Phisics', 'CompSci']
+# print(cources[0:2])
+
+# cources = ['History', 'Math', 'Phisics', 'CompSci']
+# cources.append('Art')
+# print(cources)
+
+
+# cources = ['History', 'Math', 'Phisics', 'CompSci']
+# cources.insert(0,'Art')
+# print(cources)
+
+
+
+# cources = ['History', 'Math', 'Phisics', 'CompSci']
+# cources_2 = ['Art', 'Education']
+
+# cources.insert(0,cources_2)
+# print(cources)
+
+
+# cources = ['History', 'Math', 'Phisics', 'CompSci']
+# cources_2 = ['Art', 'Education']
+
+# cources.insert(0,cources_2)
+# print(cources)
+
+
+
+# cources = ['History', 'Math', 'Phisics', 'CompSci']
+# cources_2 = ['Art', 'Education']
+
+# cources.extend(cources_2)
+# print(cources)
+
+
+# cources = ['History', 'Math', 'Phisics', 'CompSci']
+# cources_2 = ['Art', 'Education']
+
+# cources.remove('Math')
+# print(cources)
+
+
+# cources = ['History', 'Math', 'Phisics', 'CompSci']
+# cources_2 = ['Art', 'Education']
+
+# popped = cources.pop()
+# print(popped)
+
+
+
+
+# cources = ['History', 'Math', 'Phisics', 'CompSci']
+# cources_2 = ['Art', 'Education']
+
+# popped = cources.pop()
+# print(popped)
+
+
+# cources = ['History', 'Math', 'Phisics', 'CompSci']
+# cources.reverse()
+# print(cources)
+
+
+
+
+
+# cources = ['History', 'Math', 'Phisics', 'CompSci']
+
+# nums = [1,4,5,3,2]
+
+# cources.sort(reverse=True)
+
+# nums.sort(reverse=True)
+
+# print(cources)
+# print(nums)
+
+
+
+
+
+# cources = ['History', 'Math', 'Phisics', 'CompSci']
+
+# nums = [1,4,5,3,2]
+
+# sorted_courecs = sorted(cources)
+
+# print(sorted_courecs)
+# print(min(nums))
+
+
+
+# cources = ['History', 'Math', 'Phisics', 'CompSci']
+
+# nums = [1,4,5,3,2]
+
+# sorted_courecs = sorted(cources)
+
+# print(sorted_courecs)
+# print(max(nums))
+
+
+
+# cources = ['History', 'Math', 'Phisics', 'CompSci']
+
+# nums = [1,4,5,3,2]
+
+# sorted_courecs = sorted(cources)
+
+# print(sorted_courecs)
+# print(sum(nums))
+
+
+
+# cources = ['History', 'Math', 'Phisics', 'CompSci']
+
+# print(cources.index('CompSci'))
+
+
+# cources = ['History', 'Math', 'Phisics', 'CompSci']
+# print(cources.index('as'))
+
+
+
+# cources = ['History', 'Math', 'Phisics', 'CompSci']
+
+# print('Art' in cources)
+
+cources = ['History', 'Math', 'Phisics', 'CompSci']
+
+print('Math' in cources)
