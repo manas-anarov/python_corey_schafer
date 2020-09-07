5e5c1999d8a6627735077037a8c4b21ee34156ca
manas-anarov <you@example.com>
2020-09-07 11:51:52 +0600

    5lesson

--- hello.py
+++ hello.py
@@ -1,230 +1,60 @@
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-# print(len(cources))
+# student  = {'name' : 'John', 'age': 25, 'cources': ['Math', 'CompSci']}
+# print (student['cources'])
 
 
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-# print(cources[-1])
+# student  = {'name' : 'John', 'age': 25, 'cources': ['Math', 'CompSci']}
+# print (student.get('asdasd', 'Not found'))
 
 
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-# print(cources[0:2])
+# student  = {'name' : 'John', 'age': 25, 'cources': ['Math', 'CompSci']}
 
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-# cources.append('Art')
-# print(cources)
+# student['phone'] = '555-5555'
+# student['name'] = 'Jane'
+# student.update({'name' : 'Jane', age: 26, })
+# # print (student.get('phone', 'Not found'))
+# print (student)
 
 
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-# cources.insert(0,'Art')
-# print(cources)
 
+# student  = {'name' : 'John', 'age': 25, 'cources': ['Math', 'CompSci']}
 
+# student.update({'name' : 'Jane', 'age': 26, 'phone': '555-5555'})
+# print (student)
 
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-# cources_2 = ['Art', 'Education']
 
-# cources.insert(0,cources_2)
-# print(cources)
+# student  = {'name' : 'John', 'age': 25, 'cources': ['Math', 'CompSci']}
 
+# del student['age']
+# print (student)
 
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-# cources_2 = ['Art', 'Education']
 
-# cources.insert(0,cources_2)
-# print(cources)
 
+# student  = {'name' : 'John', 'age': 25, 'cources': ['Math', 'CompSci']}
 
+# age = student.pop('age')
 
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-# cources_2 = ['Art', 'Education']
 
-# cources.extend(cources_2)
-# print(cources)
+# print (student)
+# print (age)
 
 
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-# cources_2 = ['Art', 'Education']
 
-# cources.remove('Math')
-# print(cources)
 
+# student  = {'name' : 'John', 'age': 25, 'cources': ['Math', 'CompSci']}
 
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-# cources_2 = ['Art', 'Education']
+# print (len(student))
 
-# popped = cources.pop()
-# print(popped)
 
+# student  = {'name' : 'John', 'age': 25, 'cources': ['Math', 'CompSci']}
 
+# print (student.keys())
 
+# student  = {'name' : 'John', 'age': 25, 'cources': ['Math', 'CompSci']}
 
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-# cources_2 = ['Art', 'Education']
+# print (student.items())
 
-# popped = cources.pop()
-# print(popped)
 
+student  = {'name' : 'John', 'age': 25, 'cources': ['Math', 'CompSci']}
 
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-# cources.reverse()
-# print(cources)
-
-
-
-
-
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-
-# nums = [1,4,5,3,2]
-
-# cources.sort(reverse=True)
-
-# nums.sort(reverse=True)
-
-# print(cources)
-# print(nums)
-
-
-
-
-
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-
-# nums = [1,4,5,3,2]
-
-# sorted_courecs = sorted(cources)
-
-# print(sorted_courecs)
-# print(min(nums))
-
-
-
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-
-# nums = [1,4,5,3,2]
-
-# sorted_courecs = sorted(cources)
-
-# print(sorted_courecs)
-# print(max(nums))
-
-
-
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-
-# nums = [1,4,5,3,2]
-
-# sorted_courecs = sorted(cources)
-
-# print(sorted_courecs)
-# print(sum(nums))
-
-
-
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-
-# print(cources.index('CompSci'))
-
-
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-# print(cources.index('as'))
-
-
-
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-
-# print('Art' in cources)
-
-
-
-
-
-
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-# for item in cources:
-# 	print(item)
-
-
-
-
-
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-# for index, cource in enumerate(cources, start =1):
-# 	print(index, cource)
-
-
-
-
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-# course_str = ' - '.join(cources)
-# print(course_str)
-
-
-
-
-# cources = ['History', 'Math', 'Phisics', 'CompSci']
-# course_str = ' - '.join(cources)
-# new_list = course_str.split(' - ')
-
-# print(course_str)
-# print(new_list)
-
-
-
-# list_1 = ['History', 'Math', 'Phisics', 'CompSci']
-# list_2 = list_1
-
-# print(list_1)
-# print(list_2)
-
-# list_1[0] = 'Art'
-
-# print(list_1)
-# print(list_2)
-
-
-
-
-# tuple_1 = ('History', 'Math', 'Phisics', 'CompSci')
-# tuple_2 = tuple_1
-
-# print(tuple_1)
-# print(tuple_2)
-
-# tuple_1[0] = 'Art'
-
-# print(tuple_1)
-# print(tuple_2)
-
-
-
-
-# cs_sources = {'History', 'Math', 'Phisics', 'CompSci', 'Math'}
-# print('Math' in cs_sources)
-
-
-
-
-
-
-
-
-# cs_sources = {'History', 'Math', 'Phisics', 'CompSci'}
-# art_sources = {'History', 'Math', 'Art', 'Design'}
-# print(cs_sources.union(art_sources))
-
-
-
-
-
-empty_list = []
-empty_list = list()
-
-
-
-empty_tuple = ()
-empty_tuple = tuple()
-
-
-empty_set = {}
-empty_set = set()
+for key, value in student.items():
+	print (key, value)
